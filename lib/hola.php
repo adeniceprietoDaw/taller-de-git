fd6fc14af8bef141b46e09f275b37fab2d13c286
deniceDAW2 <arianad.prican@educa.jcyl.es>
2025-09-25 13:02:29 +0200

    Modificacion archivo hola.php

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,7 @@
 <?php
 //Autor: Denice < denice@gmail.com > pruebita 
 //El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+    print "Introduce tu nombre:";
+    $nombre = trim(fgets(STDIN));
+    @print "Hola, {$nombre}\n";
 ?>
